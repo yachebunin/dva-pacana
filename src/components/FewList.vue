7108603de9bb639cc21eb3dfa69512ca3a283917
chebunin yuri <chebuninyuri@gmail.com>
2023-03-23 19:09:49 +0300

    yuri-chebunin/created-app

---
+++ src/components/FewList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="block list">
+    <div
+        v-for="item in fewList"
+        :key="item.id"
+        @click="$emit('selectItem', item)"
+        class="item"
+    >
+      {{ item.name }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FewList',
+  props: ['fewList']
+}
+</script>
---
+++ src/components/FewSelected.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="few-selected block">
+    <div
+        v-if="fewSelected.length"
+        class="list"
+    >
+      <div
+          v-for="item in fewSelected"
+          :key="item.id"
+          @click="$emit('deleteItem', item)"
+          class="item"
+      >
+        {{ item.name }}
+      </div>
+    </div>
+
+    <p>selected: {{ selected || 'not selected' }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FewSelected',
+  props: ['fewSelected'],
+  computed: {
+    selected() {
+      return this.fewSelected?.map((item) => item.name).join(' / ')
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.few-selected {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+</style>
